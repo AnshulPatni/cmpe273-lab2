75bd44847ee5843d86981ca89ad9e7a223a78a15
AnshulPatni <anshulpatni19@gmail.com>
2018-09-09 23:30:19 -0700

    Added user input functionality

--- calculator_client.py
+++ calculator_client.py
@@ -10,8 +10,10 @@ def run():
     
     with grpc.insecure_channel('localhost:50051') as channel:
         stub = calculator_pb2_grpc.CalculatorStub(channel)
-        response = stub.addition(calculator_pb2.AddRequest(val1=50, val2=100))
-    print("Sum of 50 and 100 is {}".format(response.result))
+        value1 = input("Enter value 1:")
+        value2 = input("Enter value 2:")
+        response = stub.addition(calculator_pb2.AddRequest(val1=int(value1), val2=int(value2)))
+    print("Sum of the two numbers is "+str(response.result))
 
 if __name__ == '__main__':
     run()
